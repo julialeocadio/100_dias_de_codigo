6a8f0c0a3d08a4bb283388e7a0372fb0c0b2847c
julialeocadio <leocadiojulia@gmail.com>
2024-09-18 07:04:08 -0400

    modified:   Day6/calculatefactors.py 	new file:   Day7/multiplicationtable.py

--- Day6/calculatefactors.py
+++ Day6/calculatefactors.py
@@ -4,5 +4,14 @@ def factor(n):
     if n == 0 or n == 1:
         return 1
     else:
-        
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
     
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print(f"The factorial of {num} does not exist")
+else:
+    print(f"The factorial of {num} is {factor(num)}")
