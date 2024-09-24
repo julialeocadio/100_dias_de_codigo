bb2dc18fc83d54560f67cc7eafd0070b231cb49b
julialeocadio <leocadiojulia@gmail.com>
2024-09-23 20:15:44 -0400

    modified:   Day18/index.html 	modified:   Day18/main.js 	modified:   Day18/style.css 	new file:   Day19/index.html 	new file:   Day19/main.js 	new file:   Day19/style.css

---
+++ Day18/main.js
@@ -0,0 +1,54 @@
+let space = document.querySelectorAll('td');
+let msg = document.querySelector('p');
+let reset = document.querySelector('button');
+let played = 'O';
+
+space.forEach(house => {
+    house.addEventListener('click', event => {
+        if(house.innerHTML == '')
+        {
+            house.innerText = played;
+            //change play
+            played = played == 'X' ? 'O' : 'X';
+            //updating the turn message
+            msg.innerText = 'Next player: ${played}'
+            checkWin();
+        }
+    })
+})
+
+function checkWin(){
+    let tie = true;
+
+    if(
+        (space[0].innerText != '' && space[0].innerText == space[1].innerText && space[0].innerText == space[2].innerText) 
+        (space[3].innerText != '' && space[3].innerText == space[4].innerText && space[3].innerText == space[5].innerText) 
+        (space[6].innerText != '' && space[6].innerText == space[7].innerText && space[6].innerText == space[8].innerText) 
+        (space[0].innerText != '' && space[0].innerText == space[3].innerText && space[0].innerText == space[6].innerText) 
+        (space[1].innerText != '' && space[1].innerText == space[4].innerText && space[1].innerText == space[7].innerText) 
+        (space[2].innerText != '' && space[2].innerText == space[5].innerText && space[2].innerText == space[8].innerText) 
+        (space[0].innerText != '' && space[0].innerText == space[4].innerText && space[0].innerText == space[8].innerText) 
+        (space[2].innerText != '' && space[2].innerText == space[4].innerText && space[2].innerText == space[6].innerText) 
+    )
+    {
+        msg.innerHTML = '<h1>Winner!</h1';
+        tie = false;
+    }
+
+    if(tie)
+        {
+            let allFilled = true;
+            for(let i = 0; i < space.length; i++)
+            {
+                if(space[i].innerText == '')
+                {
+                    allFilled = false;
+                    break;
+                }
+            }
+            if(allFilled)
+            {
+                msg.innerHTML = '<h1>It is a tie!</h1>';
+            }
+        }
+}
